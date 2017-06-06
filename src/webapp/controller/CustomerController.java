73e44e945dc375dd5c2b65ef985d7e6a01829e33
Yinan Zhu <zynsteve@tamu.edu>
2017-06-06 13:50:57 -0400

    create the customer list

--- src/webapp/controller/CustomerController.java
+++ src/webapp/controller/CustomerController.java
@@ -1,19 +1,33 @@
 package webapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import webapp.dao.CustomerDAO;
+import webapp.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+				
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
 	
 }
-
-
---
+++ src/webapp/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package webapp.dao;
+
+import java.util.List;
+
+import webapp.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
